8e645ca915344f9cc6e3fbb323460820d6deaac6
JohnyM <mitz.jan@gmail.com>
2024-08-23 18:01:34 +0200

    adding def losovanie loterie

--- Loteria.py
+++ Loteria.py
@@ -1,16 +1,19 @@
+import random
+
+
 def stav_sportku(pocet_cisiel=6, od=1, do=49):
     vybrane_cisla = []
     print(f"Vyber si svojích {pocet_cisiel} šťastných čísiel!")
-    while len (vybrane_cisla) < pocet_cisiel:
+    while len(vybrane_cisla) < pocet_cisiel:
         cislo = input(f"Zadaj číslo od {od} do {do} bez opakovania: ")
-        #Test zadanej hodnoty, či je zadané číslo
+        # Test zadanej hodnoty, či je zadané číslo
         if cislo.isnumeric():
             cislo = int(cislo)
         else:
             print(f"Zadaná hodnota ´{cislo}´ nie je číslo, next time zadaj číslo!")
             continue
 
-        #Test interval
+        # Test interval
         if not do >= cislo >= od:
             print("Zadané číslo sa nenachádza v intervale 1-49")
             continue
@@ -20,3 +23,9 @@ def stav_sportku(pocet_cisiel=6, od=1, do=49):
         else:
             vybrane_cisla.append(cislo)
     return vybrane_cisla
+
+
+def losovanie_loterie(pocet_cisiel=6, od=1, do=49):
+    # do +1 je tam preto lebo range vyberá po predposlednú hodnotu, chcem 49 tak potrebujem 49+1
+    return random.sample(range(od, do + 1), pocet_cisiel)
+
--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
-from Loteria import stav_sportku
+from Loteria import stav_sportku, losovanie_loterie
 
 stavka = stav_sportku()
 print(stavka)
+
+losovanie = losovanie_loterie()
+print(losovanie)
