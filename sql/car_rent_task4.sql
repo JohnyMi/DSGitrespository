bce3c0273e756119fedfd7a99a2a38a5ab2b5598
JohnyMi <mitz.jan@gmail.com>
2024-09-22 13:00:56 +0200

    Join & grouby filters

--- sql/car_rent_task4.sql
+++ sql/car_rent_task4.sql
@@ -73,4 +73,62 @@ select * from bookings;
 
 select * from cars where year > 2015;
 select * from bookings where total_amount between 1000 and 2555;
-select client_id from clients where surname like 'N%' and name like '%ew'
+select client_id from clients where surname like 'N%' and name like '%ew';
+
+-- Úloha č.5 --
+-- 1. Vypište jména všech zákazníků, jejichž rezervace stála více než 1000. --
+select clients.name, clients.surname, bookings.total_amount
+from bookings
+INNER join clients on
+bookings.client_id=clients.client_id
+where total_amount > 1000; 
+
+-- 2. Vypište město bydliště všech zákazníků, kteří si pronajali auto v období 12-20.07.2020 
+-- a výkon motoru pronajatého vozu nepřesahuje 120, výsledky seřaďte podle nejvyšších nákladů na pronájem. --
+select clients.city, bookings.total_amount from clients 
+join bookings on clients.client_id = bookings.client_id
+join cars on bookings.car_id = cars.car_id
+where bookings.start_date >= '2020-07-12'
+and bookings.end_date <= '2020-07-20'
+and cars.horse_power <= 120
+order by bookings.total_amount DESC;
+
+-- 3. * Uveďte počet vozů s denními náklady na pronájem 300 a více, seskupte vozy 
+-- podle výkonu motoru a seřaďte od nejmenšího po největší.
+select clients.city, bookings.start_date, bookings.end_date, cars.horse_power, bookings.total_amount
+from bookings
+LEFT JOIN clients ON
+bookings.client_id=clients.client_id
+LEFT JOIN cars ON
+bookings.car_id=cars.car_id
+where start_date >= '2020-07-12' and end_date <= '2020-07-20' and horse_power < 120
+order by total_amount desc;
+
+-- aleternatívne riešenie
+select horse_power, count(car_id)
+from cars
+where price_per_day >= 300
+group by horse_power
+order by horse_power asc;
+
+-- 4. * Uveďte celkové náklady na všechny rezervace provedené mezi 14. a 18. červencem 2020.
+SELECT SUM(total_amount) AS total_cost
+FROM bookings
+WHERE start_date >= '2020-07-14' 
+AND end_date <= '2020-07-18';
+
+-- 5. * Vypište následující (vše v jednom dotazu):
+-- a. průměrná částka utracená každým zákazníkem název sloupce: Average_reservations_price
+-- b. počet pronajatých vozů pro každého zákazníka, přičemž se berou v úvahu pouze zákazníci, 
+-- kteří si pronajali alespoň dva vozy • sloupec: Number of rented cars
+-- c. jméno a příjmení zákazníka • sloupce: Name, Surname
+-- Seřaďte podle největšího počtu zapůjčených aut.
+select clients.name, clients.surname, 
+avg(bookings.total_amount) as 'Average_reservations_price', 
+count(bookings.car_id) as 'Number_of_rented_cars'
+from bookings
+left join clients on
+bookings.client_id=clients.client_id
+group by bookings.client_id
+having Number_of_rented_cars > 1
+order by Number_of_rented_cars desc;
